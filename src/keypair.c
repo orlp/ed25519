ef2b960fb37a18bb518ba5e1bdb97c1df167da44
Natanael de Souza Figueiredo <natanael127@users.noreply.github.com>
2018-06-26 12:26:49 -0300

    Updates to get easy for embedded systems
    - Breaking the signing and verifying process in parts for dealing with big messages with small RAM memory available.
    - New function to derivate the public key from private key, without using a seed, allowing the user to make his/her own random private key generation and then get the respective public key.

--- src/keypair.c
+++ src/keypair.c
@@ -14,3 +14,15 @@ void ed25519_create_keypair(unsigned char *public_key, unsigned char *private_ke
     ge_scalarmult_base(&A, private_key);
     ge_p3_tobytes(public_key, &A);
 }
+
+void ed25519_create_public_from_private(unsigned char *public_key, unsigned char *private_key)
+{
+    ge_p3 A;
+
+    private_key[0] &= 248;
+    private_key[31] &= 63;
+    private_key[31] |= 64;
+
+    ge_scalarmult_base(&A, private_key);
+    ge_p3_tobytes(public_key, &A);
+}
