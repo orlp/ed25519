3bafc2c6a2121dd7329d5991e762ebc91156cb62
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-10-08 23:15:27 +0200

    Prepared C# wrapper class.

---
+++ csharp/OrlpEd25519.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OrlpEd25519
+{
+    public class OrlpEd25519Context : IDisposable
+    {
+        #region Shared library loaders (per platform implementations)
+
+        private interface ISharedLibLoadUtils
+        {
+            IntPtr LoadLibrary(string fileName);
+            void FreeLibrary(IntPtr handle);
+            IntPtr GetProcAddress(IntPtr handle, string name);
+        }
+
+        private class SharedLibLoadUtilsWindows : ISharedLibLoadUtils
+        {
+            [DllImport("kernel32.dll")]
+            private static extern IntPtr LoadLibrary(string fileName);
+
+            [DllImport("kernel32.dll")]
+            private static extern int FreeLibrary(IntPtr handle);
+
+            [DllImport("kernel32.dll")]
+            private static extern IntPtr GetProcAddress(IntPtr handle, string procedureName);
+
+            void ISharedLibLoadUtils.FreeLibrary(IntPtr handle)
+            {
+                FreeLibrary(handle);
+            }
+
+            IntPtr ISharedLibLoadUtils.GetProcAddress(IntPtr dllHandle, string name)
+            {
+                return GetProcAddress(dllHandle, name);
+            }
+
+            IntPtr ISharedLibLoadUtils.LoadLibrary(string fileName)
+            {
+                return LoadLibrary(fileName);
+            }
+        }
+
+        private class SharedLibLoadUtilsLinux : ISharedLibLoadUtils
+        {
+            const int RTLD_NOW = 2;
+
+            [DllImport("libdl.so")]
+            private static extern IntPtr dlopen(String fileName, int flags);
+
+            [DllImport("libdl.so")]
+            private static extern IntPtr dlsym(IntPtr handle, String symbol);
+
+            [DllImport("libdl.so")]
+            private static extern int dlclose(IntPtr handle);
+
+            [DllImport("libdl.so")]
+            private static extern IntPtr dlerror();
+
+            public IntPtr LoadLibrary(string fileName)
+            {
+                return dlopen(fileName, RTLD_NOW);
+            }
+
+            public void FreeLibrary(IntPtr handle)
+            {
+                dlclose(handle);
+            }
+
+            public IntPtr GetProcAddress(IntPtr dllHandle, string name)
+            {
+                dlerror();
+                IntPtr res = dlsym(dllHandle, name);
+                IntPtr err = dlerror();
+                if (err != IntPtr.Zero)
+                {
+                    throw new Exception("dlsym: " + Marshal.PtrToStringAnsi(err));
+                }
+
+                return res;
+            }
+        }
+
+        private class SharedLibLoadUtilsMac : ISharedLibLoadUtils
+        {
+            const int RTLD_NOW = 2;
+
+            [DllImport("libdl.dylib")]
+            private static extern IntPtr dlopen(String fileName, int flags);
+
+            [DllImport("libdl.dylib")]
+            private static extern IntPtr dlsym(IntPtr handle, String symbol);
+
+            [DllImport("libdl.dylib")]
+            private static extern int dlclose(IntPtr handle);
+
+            [DllImport("libdl.dylib")]
+            private static extern IntPtr dlerror();
+
+            public IntPtr LoadLibrary(string fileName)
+            {
+                return dlopen(fileName, RTLD_NOW);
+            }
+
+            public void FreeLibrary(IntPtr handle)
+            {
+                dlclose(handle);
+            }
+
+            public IntPtr GetProcAddress(IntPtr dllHandle, string name)
+            {
+                dlerror();
+                IntPtr res = dlsym(dllHandle, name);
+                IntPtr err = dlerror();
+                if (err != IntPtr.Zero)
+                {
+                    throw new Exception("dlsym: " + Marshal.PtrToStringAnsi(err));
+                }
+
+                return res;
+            }
+        }
+
+        #endregion
+
+        #region Function mapping
+
+        // TODO: function mapping (delegates)
+        
+        #endregion
+
+        private IntPtr lib;
+        private ISharedLibLoadUtils loadUtils = null;
+
+        /// <summary>
+        /// Absolute path to the orlp-ed25519 shared library that is currently loaded into memory.
+        /// </summary>
+        public string LoadedLibraryPath { get; }
+
+        public OrlpEd25519Context(string sharedLibPathOverride = null)
+        {
+            string os;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                os = "windows";
+                loadUtils = new SharedLibLoadUtilsWindows();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                os = "linux";
+                loadUtils = new SharedLibLoadUtilsLinux();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                os = "mac";
+                loadUtils = new SharedLibLoadUtilsMac();
+            }
+            else
+            {
+                throw new PlatformNotSupportedException("Unsupported OS");
+            }
+
+            if (string.IsNullOrEmpty(sharedLibPathOverride))
+            {
+                StringBuilder pathBuilder = new StringBuilder(256);
+                pathBuilder.Append("lib/");
+
+                switch (RuntimeInformation.ProcessArchitecture)
+                {
+                    case Architecture.X64:
+                        pathBuilder.Append("x64/");
+                        break;
+                    case Architecture.X86:
+                        pathBuilder.Append("x86/");
+                        break;
+                    case Architecture.Arm:
+                        pathBuilder.Append("armeabi-v7a/");
+                        break;
+                    case Architecture.Arm64:
+                        pathBuilder.Append("arm64-v8a/");
+                        break;
+                }
+
+                if (!Directory.Exists(pathBuilder.ToString()))
+                {
+                    throw new PlatformNotSupportedException($"orlp-ed25519 shared library not found in {pathBuilder.ToString()} and/or unsupported CPU architecture. Please don't forget to copy the orlp-ed25519 shared libraries/DLL into the 'lib/{{CPU_ARCHITECTURE}}/{{OS}}/{{SHARED_LIB_FILE}}' folder of your output build directory.");
+                }
+
+                pathBuilder.Append(os);
+                pathBuilder.Append('/');
+
+                string[] l = Directory.GetFiles(pathBuilder.ToString());
+                if (l == null || l.Length != 1)
+                {
+                    throw new FileLoadException("There should only be exactly one orlp-ed25519 shared library file per supported platform!");
+                }
+
+                pathBuilder.Append(Path.GetFileName(l[0]));
+                LoadedLibraryPath = Path.GetFullPath(pathBuilder.ToString());
+                pathBuilder.Clear();
+            }
+            else
+            {
+                LoadedLibraryPath = sharedLibPathOverride;
+            }
+
+            lib = loadUtils.LoadLibrary(LoadedLibraryPath);
+            if (lib == IntPtr.Zero)
+            {
+                goto hell;
+            }
+
+            // TODO: load functions from DLL here
+            
+            return;
+
+            hell:
+            throw new Exception($"Failed to load one or more functions from the orlp-ed25519 shared library \"{LoadedLibraryPath}\"!");
+        }
+
+        public void Dispose()
+        {
+            loadUtils.FreeLibrary(lib);
+        }
+    }
+
+    internal class OrlpEd25519ContextExample
+    {
+        static void Main()
+        {
+            Console.WriteLine("--- orlp-ed25519 ---\n");
+        }
+    }
+}
